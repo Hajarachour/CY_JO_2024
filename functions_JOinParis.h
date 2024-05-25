d5e27a364082c374895affbeabf5ccefc333abb6
Dimittri <155728681+arjyoo@users.noreply.github.com>
2024-05-25 20:59:12 +0200

    Update functions_JOinParis.h

--- functions_JOinParis.h
+++ functions_JOinParis.h
@@ -8,6 +8,7 @@ void create_data_dir();
 void create_athlete_file(const char* athlete_name);
 void add_performance(const char* athlete_name, const char* date, const char* event, const char* time, int position);
 void create_initial_athletes();
+int compare_dates(const void *a, const void *b);
 void read_performances(const char* athlete_name, Athlete *athlete);
 void display_history(const Athlete *athlete);
 void athlete_statistics(const Athlete *athlete, const char *event);
@@ -21,5 +22,6 @@ int is_valid_sport(const char *sport);
 int is_valid_position(int position);
 int check_exiting_relais_for_date(const char *athlete_name, char *date_event);
 void find_top_athletes(const char *event);
+int compare_performance(const void *a, const void *b);
 
 #endif
